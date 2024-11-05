47a2d173925ae5f1f07bfbcb62bafc02ac70e8f0
BlueBird <112836850+cbluebird@users.noreply.github.com>
2024-11-05 10:33:43 +0800

    feat: init user, gateway and post modules
    Signed-off-by: cbluebird <crk2778355976@163.com>

---
+++ post/src/main/java/org/jh/post/PostApplication.java
@@ -0,0 +1,17 @@
+package org.jh.post;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableFeignClients
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PostApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PostApplication.class, args);
+    }
+
+}
