309426eb8af018f5b1d01d6873cd67ba25ab3913
cbluebird <crk2778355976@163.com>
2024-12-13 00:02:28 +0800

    feat: add collection controller
    Signed-off-by: cbluebird <crk2778355976@163.com>

---
+++ forum-post/src/main/java/org/jh/forum/post/controller/CollectionController.java
@@ -0,0 +1,73 @@
+package org.jh.forum.post.controller;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import org.jh.forum.post.model.Collection;
+import org.jh.forum.post.model.Post;
+import org.jh.forum.post.service.ICollectionService;
+import org.jh.forum.post.service.IPostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Validated
+@RestController
+@RequestMapping("/api/post/collection")
+public class CollectionController {
+
+    @Autowired
+    private ICollectionService collectionService;
+
+    @Autowired
+    private IPostService postService;
+
+    @PostMapping("/add")
+    public void addCollection(@RequestBody Post post, @RequestHeader("X-User-ID") String userId) {
+        Long userIdLong = Long.valueOf(userId);
+        QueryWrapper<Collection> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("post_id", post.getId()).eq("user_id", userIdLong);
+
+        Collection existingCollection = collectionService.getOne(queryWrapper);
+        if (existingCollection == null) {
+            Collection collection = new Collection();
+            collection.setPostId(post.getId());
+            collection.setUserId(userIdLong);
+            collection.setCreatedOn(new Date());
+            collectionService.save(collection);
+
+            post.setCollectionCount(post.getCollectionCount() + 1);
+            postService.updateById(post);
+        }
+    }
+
+    @DeleteMapping("/del")
+    public void removeCollection(@RequestBody Post post, @RequestHeader("X-User-ID") String userId) {
+        Long userIdLong = Long.valueOf(userId);
+        QueryWrapper<Collection> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("post_id", post.getId()).eq("user_id", userIdLong);
+
+        Collection existingCollection = collectionService.getOne(queryWrapper);
+        if (existingCollection != null) {
+            collectionService.remove(queryWrapper);
+
+            post.setCollectionCount(post.getCollectionCount() - 1);
+            postService.updateById(post);
+        }
+    }
+
+    @GetMapping("/get")
+    public List<Post> getPostCollectionPage(@RequestParam int page_num, @RequestParam int page_size, @RequestHeader("X-User-ID") String userId) {
+        Long userIdLong = Long.valueOf(userId);
+        QueryWrapper<Collection> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("user_id", userIdLong).orderByDesc("created_on");
+
+        Page<Collection> collectionPage = collectionService.page(new Page<>(page_num, page_size), queryWrapper);
+        List<Long> postIds = collectionPage.getRecords().stream().map(Collection::getPostId).collect(Collectors.toList());
+
+        return postService.listByIds(postIds);
+    }
+}
