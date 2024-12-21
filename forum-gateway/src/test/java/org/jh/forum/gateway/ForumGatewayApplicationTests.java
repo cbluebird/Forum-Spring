1d8ef71491c87d1cf8ec93ad4920bf59c8ecc454
cbluebird <crk2778355976@163.com>
2024-12-21 21:49:19 +0800

    bugfix: fix the bug when package
    Signed-off-by: cbluebird <crk2778355976@163.com>

--- forum-gateway/src/test/java/org/jh/forum/gateway/ForumGatewayApplicationTests.java
+++ forum-gateway/src/test/java/org/jh/forum/gateway/ForumGatewayApplicationTests.java
@@ -1,13 +1,7 @@
 package org.jh.forum.gateway;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ForumGatewayApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
 }
--- forum-user/src/test/java/org/jh/forum/user/ForumUserApplicationTests.java
+++ forum-user/src/test/java/org/jh/forum/user/ForumUserApplicationTests.java
@@ -1,13 +1,7 @@
 package org.jh.forum.user;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ForumUserApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
 }
